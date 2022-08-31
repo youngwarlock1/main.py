0d583d85413119bbe7654a7ac2c307d670487c6f
youngwarlock1 <112525779+youngwarlock1@users.noreply.github.com>
2022-08-31 02:01:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="BUSINESS OPPORTUNITY")
+st.sidebar.header("Sidebar")
+
+st.subheader("Hi guys!")
+st.header('You are highly:')
+st.title("Welcome to IBRAHIM UJULU's website")
+st.write("I am going to show you how to make money online with 'SFI' (STRONG FUTURE INTERNATIONAL). " 
+         " SFI is one of today's biggest world wide business that you can make money from home using your computer, tablet ,android e.t.c ")
+st.write("[learn more about my business or click here to sing_up now>](https://www.sfi4.com/21751728/FREE)")
+st.markdown("i am a boy the willing time")
